65323d8e8a25f2298b2608ac86e8b91e842dbd5e
s-r-aman <s.r.aman.sra@gmail.com>
2018-10-18 20:45:09 +0530

    added OneVsAll Classifier.

--- week_4/week4.m
+++ week_4/week4.m
@@ -35,3 +35,19 @@ X_t = [ones(5,1) reshape(1:15,5,3)/10];
 y_t = ([1;0;1;0;1] >= 0.5);
 lambda_t = 3;
 [J grad] = lrCostFunction(theta_t, X_t, y_t, lambda_t)
+
+function [all_theta] = oneVsAll(X, y, num_labels, lambda)
+
+m = size(X, 1);
+n = size(X, 2);
+
+all_theta = zeros(num_labels, n + 1);
+
+X = [ones(m, 1) X];
+initial_theta = zeros(n + 1, 1);
+
+options = optimset('GradObj', 'on', 'MaxIter', 50);
+
+for c = 1:num_labels
+    all_theta(c,:) = fmincg (@(t)(lrCostFunction(t, X, (y == c), lambda)), initial_theta, options);
+end
